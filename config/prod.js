9cb408cc68e4e4093a9daba579b257a5edd6fa1b
keerthanaysk <96902795+keerthanaysk@users.noreply.github.com>
2025-06-23 01:33:04 +0530

    Replace old project with new version

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+//prod.js - prodcution key is here
+
+
+
+module.exports = {
+    googleClientID: 'process.env.GOOGLE_CLIENT_ID',
+    googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
+    mongoURI:'process.env._URI',
+    cookieKey:'process.env.COOKIE_KEY'  
+}
